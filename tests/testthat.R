a7497b9852744fcce564fb3a9c32780a9c143eb5
Will <willi@mmorris.email>
2019-06-06 11:12:20 +0000

    Add unit tests for finbif_token

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(finbif)
+
+test_check("finbif")
---
+++ tests/testthat/test-finbif_token.R
@@ -0,0 +1,14 @@
+context("FinBIF access token")
+
+test_that("Access token is a string", {
+  token <- Sys.getenv("FINBIF_ACCESS_TOKEN")
+  if (identical(token, "")) {
+    expect_null(finbif_token())
+  } else {
+    expect_s3_class(finbif_token(), "character")
+  }
+})
+
+test_that("Access token retrival produces a message", {
+  expect_message(finbif_token(quiet = FALSE))
+})
