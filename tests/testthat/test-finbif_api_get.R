f1e2568242f10fa0993a937deec4303c1c3cd319
Will <willi@mmorris.email>
2019-06-28 08:07:05 +0000

    Update tests for finbif_api_get

--- tests/testthat/test-finbif_api_get.R
+++ tests/testthat/test-finbif_api_get.R
@@ -1,13 +1,28 @@
 context('FinBIF api get request')
 
-test_that("returns valid data", {
-  vcr::use_cassette("finbif_api_get", {
-    resp_list <- finbif_api_get(
+test_that("missing token returns error", {
+  token <- Sys.getenv("FINBIF_ACCESS_TOKEN")
+  Sys.unsetenv("FINBIF_ACCESS_TOKEN")
+  expect_error(finbif_api_get(), "Access token for FinBIF has not been set")
+  Sys.setenv(FINBIF_ACCESS_TOKEN = token)
+})
+
+vcr::use_cassette("finbif_api_get", {
+  test_that("returns valid data", {
+    resp_list1 <- finbif_api_get(
       path = "v0/warehouse/query/list",
       query = list(page = 1, pageSize = 1, selected = "unit.unitId")
     )
-  }, preserve_exact_body_bytes = TRUE)
+    expect_s3_class(resp_list1, "finbif_api")
+  })
 
-  expect_s3_class(resp_list, "finbif_api")
-
-})
+  test_that("with wrong field returns an error message", {
+    expect_error(
+      finbif_api_get(
+        path = "v0/warehouse/query/list",
+        query = list(page = 1, pageSize = 1, selected = "not_a_field")
+      ),
+      "API request failed"
+    )
+  })
+}, preserve_exact_body_bytes = TRUE)
