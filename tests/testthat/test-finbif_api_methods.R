54fb38b04c03ead02914a5cd2fe2d8b318562661
Will <willi@mmorris.email>
2019-06-28 08:06:40 +0000

    Update tests for finbif_api methods

--- tests/testthat/test-finbif_api_methods.R
+++ tests/testthat/test-finbif_api_methods.R
@@ -1,13 +1,13 @@
 context("FinBIF api methods")
 
 test_that("are working", {
-  vcr::use_cassette("finbif_api_get", {
-    resp_list <- finbif_api_get(
-      path = "v0/warehouse/query/list",
-      query = list(page = 1, pageSize = 1, selected = "unit.unitId")
-    )
+  vcr::use_cassette("finbif_records", {
+    resp_list_n1 <- finbif_records(n = 1)
+    resp_list_n101 <- finbif_records(n = 101)
   }, preserve_exact_body_bytes = TRUE)
-  expect_output(resp_list_2 <- print(resp_list), "FinBIF")
-  expect_identical(resp_list_2, resp_list)
-  expect_s3_class(as.data.frame(resp_list), "data.frame")
+
+  expect_output(print(resp_list_n101[[1]]), "FinBIF")
+  expect_output(resp_list_n1_2 <- print(resp_list_n1), "FinBIF")
+  expect_identical(resp_list_n1_2, resp_list_n1)
+  expect_s3_class(as.data.frame(resp_list_n101), "data.frame")
 })
