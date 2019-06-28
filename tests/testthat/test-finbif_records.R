865362212d2f5c91a6e93d9aa4a51550e907200a
Will <willi@mmorris.email>
2019-06-28 06:39:03 +0000

    Tests for updated finbif_records

--- tests/testthat/test-finbif_records.R
+++ tests/testthat/test-finbif_records.R
@@ -2,9 +2,12 @@ context('Downloading FinBIF records')
 
 test_that("returns valid data", {
   vcr::use_cassette("finbif_records", {
-    resp_list <- finbif_records(n = 1)
+    resp_list_n1 <- finbif_records(n = 1)
+    resp_list_n101 <- finbif_records(n = 101)
   }, preserve_exact_body_bytes = TRUE)
 
-  expect_s3_class(resp_list[[1]], "finbif_api")
-
+  expect_s3_class(resp_list_n1[[1]], "finbif_api")
+  expect_s3_class(resp_list_n101[[1]], "finbif_api")
+  expect_s3_class(resp_list_n101[[2]], "finbif_api")
+  expect_error(finbif_records(n=1e99), "Cannot download more than")
 })
