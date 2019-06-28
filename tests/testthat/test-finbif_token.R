748a14c248f8eaced0e1516fd26d8d72aedf5206
Will <willi@mmorris.email>
2019-06-28 06:22:09 +0000

    Test behaviour when access token unset

--- tests/testthat/test-finbif_token.R
+++ tests/testthat/test-finbif_token.R
@@ -12,3 +12,10 @@ test_that("Access token is a string", {
 test_that("Access token retrival produces a message", {
   expect_message(finbif_token(quiet = FALSE))
 })
+
+test_that("Access token returns NULL when unset", {
+  token <- Sys.getenv("FINBIF_ACCESS_TOKEN")
+  Sys.unsetenv("FINBIF_ACCESS_TOKEN")
+  expect_null(finbif_token())
+  Sys.setenv(FINBIF_ACCESS_TOKEN = token)
+})
