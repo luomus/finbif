abab9aac97743bd2f8a48d37c1f4b1621f91ad18
Will <willi@mmorris.email>
2019-07-22 13:52:14 +0000

    Fix display of admin status

--- R/finbif_admin_status.R
+++ R/finbif_admin_status.R
@@ -12,6 +12,6 @@
 finbif_admin_status <- function() {
   df <- admin_status_translations
   nrows <- nrow(admin_status_translations)
-  df <- structure(df, row.names = seq_len(nrows), names = c("Status", "Code"))
-  df[order(df[["Status"]]), ]
+  df <- df[order(df[["translated_status"]]), ]
+  structure(df, row.names = seq_len(nrows), names = c("status", "code"))
 }
