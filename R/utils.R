475b57a0095f13cedd7e811897e4bbfd01a06531
Will <willi@mmorris.email>
2019-07-30 06:22:32 +0000

    Use a recursive version of get_next_lowest_factor

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,5 @@ to_sentence_case <- function(string) {
   paste0(substring(toupper(string), 1L, 1L), substring(tolower(string), 2L))
 }
 
-get_next_lowest_factor <- function(x, y) {
-  while (x %% y) y <- y - 1L
-  y
-}
+get_next_lowest_factor <-
+  function(x, y) ifelse(x %% y, get_next_lowest_factor(x, y - 1L), y)
