bd04a587c58705727440648df065964eafb481c1
Will <willi@mmorris.email>
2019-08-05 07:32:13 +0000

    Minor code style fix

--- data-raw/informal_groups.R
+++ data-raw/informal_groups.R
@@ -2,7 +2,9 @@ informal_groups <- finbif:::finbif_api_get(
   "v0/informal-taxon-groups/tree", list(pageSize = 1000), FALSE
 )
 informal_groups <- data.tree::as.Node(informal_groups$content$results)
-informal_groups <- data.tree::ToDataFrameTree(informal_groups, "pathString", "id")
+informal_groups <- data.tree::ToDataFrameTree(
+  informal_groups, "pathString", "id"
+)
 informal_groups <- informal_groups[!is.na(informal_groups[["id"]]), ]
 informal_groups$pathString <- gsub(
   "hasSubGroup/", "", informal_groups$pathString
