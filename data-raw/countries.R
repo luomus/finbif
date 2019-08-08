eec62dc0687a829e95dc89779b08d3f958a09375
Will <willi@mmorris.email>
2019-08-08 12:14:50 +0000

    Fix country column names

--- data-raw/countries.R
+++ data-raw/countries.R
@@ -20,4 +20,4 @@ countries <- countries[
   order(countries["name.en"]),
   c("name.en", "name.fi", "countryCodeISOalpha2", "countryCodeISOalpha3")
 ]
-names(countries) <- c("name_fi", "name_en", "alpha2", "alpha3")
+names(countries) <- c("name_en", "name_fi", "alpha2", "alpha3")
