c3ded706c3b585461d7a3b234a202c25c0607492
Will <willi@mmorris.email>
2019-09-23 09:05:02 +0000

    Add missing variables

--- data-raw/variables.R
+++ data-raw/variables.R
@@ -3,4 +3,12 @@ var_names <- read.csv(
   row.names = 1L
 )
 
+vars <- httr::GET("https://api.laji.fi/explorer/swagger.json")
+vars <- jsonlite::fromJSON(httr::content(vars, "text"), simplifyVector = FALSE)
+vars <- vars[["paths"]][["/warehouse/query/list"]][["get"]][["parameters"]]
+vars <- vars[[which(vapply(vars, getElement, "", "name") == "selected")]]
+vars <- unlist(vars[["items"]][["enum"]])
+
+stopifnot(identical(sort(row.names(var_names)), sort(vars)))
+
 class(var_names[["translated_var"]]) <- "translation"
