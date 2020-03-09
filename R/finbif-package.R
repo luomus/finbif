0ef08c60666614230a55bac9e79ebe9bb92caf87
Will <willi@mmorris.email>
2020-03-09 12:29:44 +0000

    Add package options to package docs

--- R/finbif-package.R
+++ R/finbif-package.R
@@ -1,4 +1,17 @@
-#' @keywords internal
+#' @section Package options:
+#' \describe{
+#' \item{`finbif_api_url`}{Character. The base url of the API to query. Default:
+#'   `"api.laji.fi"`}
+#' \item{`finbif_api_version`}{Character. The API version to use. Default:
+#'   `"v0"`}
+#' \item{`finbif_use_cache`}{Logical. Should API queries by cached. Default:
+#'   `TRUE`}
+#' \item{`finbif_cache_path`}{The path to the directory where to store cached
+#'   API queries. If unset (the default) in memory caching is used.}
+#' \item{`finbif_tz`}{The timezone used by `finbif` functions that compute dates
+#'   and times. Default: `Sys.timezone()`}
+#'
+#' }
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
@@ -6,3 +19,5 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+
