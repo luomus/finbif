3a75b5450e0899e8d19fae2f305208672f427e7d
Will <willi@mmorris.email>
2020-04-14 14:56:43 +0000

    Update dev test cassettes

--- tests/testthat/helper-finbif.R
+++ tests/testthat/helper-finbif.R
@@ -4,7 +4,8 @@ invisible(vcr::vcr_configure(
   dir = "../cassettes",
   record = "new_episodes",
   filter_sensitive_data = list(
-    "<finbif_token>" = Sys.getenv("FINBIF_ACCESS_TOKEN")
+    "<finbif_token>" = Sys.getenv("FINBIF_ACCESS_TOKEN"),
+    "<finbif_dev_token>" = Sys.getenv("FINBIF_DEV_ACCESS_TOKEN")
   )
 ))
 
