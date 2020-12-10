16f7aefe783805952c5b30b098a5af74605b369c
Will <willi@mmorris.email>
2020-12-10 14:51:04 +0000

    Update test artifacts

--- tests/testthat/helper-finbif.R
+++ tests/testthat/helper-finbif.R
@@ -7,7 +7,6 @@ if (requireNamespace("vcr")) {
     vcr::vcr_configure(
       dir = "../cassettes",
       record = "new_episodes",
-      clean_outdated_http_interactions = TRUE,
       filter_sensitive_data = list(
         "<finbif_token>"     = Sys.getenv("FINBIF_ACCESS_TOKEN"),
         "<finbif_dev_token>" = Sys.getenv("FINBIF_DEV_ACCESS_TOKEN")
