1b5612834bbf139d4ff42d413cfdb942fa01c614
wkmor1 <willi@mmorris.email>
2022-10-29 13:32:04 +0300

    Lint atlas raw data script

--- data-raw/atlas.R
+++ data-raw/atlas.R
@@ -2,7 +2,8 @@ source("data-raw/utils.R")
 metadata_ranges <-
   finbif:::api_get("metadata/ranges", list(lang = "multi"), FALSE)[["content"]]
 atlas_code <- lapply(
-  metadata_ranges[["MY.atlasCodeEnum"]],  as.data.frame, stringsAsFactors = FALSE
+  metadata_ranges[["MY.atlasCodeEnum"]],  as.data.frame,
+  stringsAsFactors = FALSE
 )
 atlas_code <- reduce_merge(atlas_code)
 row.names(atlas_code) <- atlas_code[["id"]]
