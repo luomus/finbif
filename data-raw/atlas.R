351ea60a4d028ffbacb95f9088819a766a5a6a4f
wkmor1 <willi@mmorris.email>
2022-11-24 09:39:28 +0200

    Fix atlas code sysdata

--- data-raw/atlas.R
+++ data-raw/atlas.R
@@ -9,8 +9,7 @@ atlas_code <- reduce_merge(atlas_code)
 row.names(atlas_code) <- atlas_code[["id"]]
 atlas_code[["id"]] <- NULL
 
-cols <- c("name_en", "name_fi", "name_sv")
-names(atlas_code) <- cols
+names(atlas_code) <- sub("value\\.", "name_", names(atlas_code))
 
 atlas_class <- lapply(
   metadata_ranges[["MY.atlasClassEnum"]], as.data.frame,
@@ -19,9 +18,9 @@ atlas_class <- lapply(
 atlas_class <- reduce_merge(atlas_class)
 row.names(atlas_class) <- atlas_class[["id"]]
 atlas_class[["id"]] <- NULL
-names(atlas_class) <- cols
+names(atlas_class) <- sub("value\\.", "name_", names(atlas_class))
 
-for (i in cols) {
+for (i in names(atlas_code)) {
 
   class(atlas_code[[i]]) <- "translation"
   class(atlas_class[[i]]) <- "translation"
