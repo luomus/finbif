913cad7be6cc51e19d6849375665375551db5da6
wkmor1 <willi@mmorris.email>
2023-01-19 15:00:46 +0200

    Lint finbif_clear_cache

--- R/finbif_clear_cache.R
+++ R/finbif_clear_cache.R
@@ -7,18 +7,34 @@
 #' @examples \dontrun{
 #'
 #' finbif_clear_cache()
+#'
 #' }
 #' @export
 
 finbif_clear_cache <- function() {
+
   fcp <- getOption("finbif_cache_path")
-  if (is.null(fcp)) {
-    rm(
-      list = ls(all.names = TRUE, envir = cache_location),
-      envir = cache_location
-    )
+
+  fcp_is_null <- is.null(fcp)
+
+  if (fcp_is_null) {
+
+    cache_list <- ls(all.names = TRUE, envir = cache_location)
+
+    rm(list = cache_list, envir = cache_location)
+
   } else {
-    stopifnot(identical(unlink(file.path(fcp, "finbif_cache_file_*")), 0L))
+
+    cache_files <- file.path(fcp, "finbif_cache_file_*")
+
+    cache_files_deleted <- unlink(cache_files)
+
+    cache_files_deleted <- identical(cache_files_deleted, 0L)
+
+    stopifnot(cache_files_deleted)
+
     invisible(NULL)
+
   }
+
 }
