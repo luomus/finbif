0d3cde2775edd0e1441f6a29aca645c2491cb44d
wkmor1 <willi@mmorris.email>
2023-01-23 13:17:11 +0200

    Lint token

--- R/token.R
+++ R/token.R
@@ -1,12 +1,27 @@
 #' @noRd
 
-token <- function(quiet = TRUE) {
+token <- function(
+  quiet = TRUE
+) {
+
   finbif_access_token <- Sys.getenv("FINBIF_ACCESS_TOKEN")
+
   if (!quiet) {
+
     message(
       "Using FinBIF access token from environment variable FINBIF_ACCESS_TOKEN"
     )
+
   }
-  if (identical(finbif_access_token, "")) return(NULL)
+
+  no_token <- identical(finbif_access_token, "")
+
+  if (no_token) {
+
+    finbif_access_token <- NULL
+
+  }
+
   finbif_access_token
+
 }
