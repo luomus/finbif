9aaabe76a166dc226cd211cad9f7d1eb315855f4
wkmor1 <willi@mmorris.email>
2023-01-23 13:30:09 +0200

    Lint zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,16 @@
-.onLoad <- function(libname, pkgname) { # nolint
+#' @noRd
+
+.onLoad <- function(libname, pkgname) {
+
   op <- options()
+
+  op_names <- names(op)
+
+  locale <- get_locale()
+
+  tz <- Sys.timezone()
+
   op_finbif <- list(
-    # There are (or will be) multiple versions and locations of the FinBIF API
     finbif_api_url = "https://api.laji.fi",
     finbif_dl_url = "https://dw.laji.fi/download",
     finbif_api_version = "v0",
@@ -11,19 +20,46 @@
     finbif_rate_limit = 1,
     finbif_max_queries = 2000L,
     finbif_max_page_size = 1000L,
-    finbif_retry_times = 3,
-    finbif_retry_pause_base = 1,
-    finbif_retry_pause_cap = 60,
-    finbif_retry_pause_min = 1,
+    finbif_retry_times = 3L,
+    finbif_retry_pause_base = 1L,
+    finbif_retry_pause_cap = 60L,
+    finbif_retry_pause_min = 1L,
     finbif_use_cache = TRUE,
-    finbif_tz = Sys.timezone(),
-    finbif_locale = get_locale()
+    finbif_tz = tz,
+    finbif_locale = locale
   )
-  toset <- !(names(op_finbif) %in% names(op))
-  if (any(toset)) options(op_finbif[toset])
+
+  op_finbif_names <- names(op_finbif)
+
+  toset <- !op_finbif_names %in% op_names
+
+  any_toset <- any(toset)
+
+  if (any_toset) {
+
+    op_finbif_toset <- op_finbif[toset]
+
+    options(op_finbif_toset)
+
+  }
+
   invisible()
+
 }
 
+#' @noRd
 #' @exportPattern ^fb
-for (fn in ls(pattern = "^finbif")) assign(gsub("finbif", "fb", fn), get(fn))
-rm(fn)
+
+fns <- ls(pattern = "^finbif")
+
+for (fn in fns) {
+
+  new_name <- gsub("finbif", "fb", fn)
+
+  fn_obj <- get(fn)
+
+  assign(new_name, fn_obj)
+
+}
+
+rm(fn, fns, new_name, fn_obj)
