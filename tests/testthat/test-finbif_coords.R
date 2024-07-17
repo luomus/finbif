f059e2f43ace5058bd1400f5010f1bb2fbc21110
wkmor1 <willi@mmorris.email>
2024-07-17 09:50:51 +0300

    Remove old tests

--- tests/testthat/test-finbif_coords.R
+++
@@ -1,8 +0,0 @@
-test_that(
-  "returns valid data", {
-
-    expect_type(coords(list(67, 32, "ykj")), "character")
-    expect_type(coords(list(67, 32, "ykj", 1)), "character")
-
-  }
-)
--- tests/testthat/test-onload.R
+++
@@ -1,7 +0,0 @@
-test_that(
-  "works", {
-    options("finbif_use_cache" = NULL)
-    finbif:::.onLoad()
-    expect_type(getOption("finbif_use_cache"), "logical")
-  }
-)
