db6dc95c604ae9365d6e159b81b0739a84ac9001
wkmor1 <willi@mmorris.email>
2025-03-17 12:48:25 +0200

    Remove vertical whitespace from fb_clear_cache

--- R/finbif_clear_cache.R
+++ R/finbif_clear_cache.R
@@ -10,39 +10,25 @@
 #'
 #' }
 #' @export
-
 finbif_clear_cache <- function() {
-
   fcp <- getOption("finbif_cache_path")
 
   if (is.null(fcp)) {
-
     cache_list <- ls(all.names = TRUE, envir = cache_location)
-
     rm(list = cache_list, envir = cache_location)
-
   } else if (is.character(fcp)) {
-
     cache_files <- file.path(fcp, "finbif_cache_file_*")
-
     cache_files_deleted <- unlink(cache_files)
-
     cache_files_deleted <- identical(cache_files_deleted, 0L)
-
     stopifnot("Cache file deletion failed" = cache_files_deleted)
-
   } else {
-
     stopifnot("Package {DBI} needed to use a DB cache" = has_pkgs("DBI"))
 
     if (DBI::dbExistsTable(fcp, "finbif_cache")) {
-
       DBI::dbRemoveTable(fcp, "finbif_cache")
-
     }
 
   }
 
   invisible(NULL)
-
 }
