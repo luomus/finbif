924eaf1cfc84246219c730aba15966b179c2a69c
wkmor1 <willi@mmorris.email>
2026-06-30 15:16:59 +0300

    Use legalcode for license refs

--- R/sysdata.R
+++ R/sysdata.R
@@ -1029,49 +1029,49 @@ license <- function() {
   licenses <- list(
     name_en = c(
       "http://tun.fi/MY.intellectualRightsCC-BY" =
-        "https://creativecommons.org/licenses/by/4.0/",
+        "https://creativecommons.org/licenses/by/4.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC-BY" =
-        "https://creativecommons.org/licenses/by/4.0/",
+        "https://creativecommons.org/licenses/by/4.0/legalcode",
       "http://tun.fi/MY.intellectualRightsCC-BY-4.0" =
-        "https://creativecommons.org/licenses/by/4.0/",
+        "https://creativecommons.org/licenses/by/4.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC-BY-4.0" =
-        "https://creativecommons.org/licenses/by/4.0/",
+        "https://creativecommons.org/licenses/by/4.0/legalcode",
       "http://tun.fi/MY.intellectualRightsCC0" =
-        "https://creativecommons.org/publicdomain/zero/1.0/",
+        "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC0" =
-        "https://creativecommons.org/publicdomain/zero/1.0/",
+        "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
       "http://tun.fi/MY.intellectualRightsCC0-4.0" =
-        "https://creativecommons.org/publicdomain/zero/1.0/",
+        "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC0-4.0" =
-        "https://creativecommons.org/publicdomain/zero/1.0/",
+        "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
       "http://tun.fi/MY.intellectualRightsPD" =
-        "https://creativecommons.org/publicdomain/zero/1.0/",
+        "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsPD" =
-        "https://creativecommons.org/publicdomain/zero/1.0/",
+        "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
       "http://tun.fi/MY.intellectualRightsARR" =
         "All Rights Reserved",
       "http://tun.fi/MZ.intellectualRightsARR" =
         "All Rights Reserved",
       "http://tun.fi/MY.intellectualRightsCC-BY-NC-4.0" =
-        "https://creativecommons.org/licenses/by-nc/4.0/",
+        "https://creativecommons.org/licenses/by-nc/4.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC-BY-NC-4.0" =
-        "https://creativecommons.org/licenses/by-nc/4.0/",
+        "https://creativecommons.org/licenses/by-nc/4.0/legalcode",
       "http://tun.fi/MY.intellectualRightsCC-BY-NC-SA-4.0" =
-        "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+        "https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC-BY-NC-SA-4.0" =
-        "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+        "https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode",
       "http://tun.fi/MY.intellectualRightsCC-BY-SA-4.0" =
-        "https://creativecommons.org/licenses/by-sa/4.0/",
+        "https://creativecommons.org/licenses/by-sa/4.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC-BY-SA-4.0" =
-        "https://creativecommons.org/licenses/by-sa/4.0/",
+        "https://creativecommons.org/licenses/by-sa/4.0/legalcode",
       "http://tun.fi/MY.intellectualRightsCC-BY-NC-ND-4.0" =
-        "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+        "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC-BY-NC-ND-4.0" =
-        "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+        "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode",
       "http://tun.fi/MY.intellectualRightsCC-BY-ND-4.0" =
-        "https://creativecommons.org/licenses/by-nd/4.0/",
+        "https://creativecommons.org/licenses/by-nd/4.0/legalcode",
       "http://tun.fi/MZ.intellectualRightsCC-BY-ND-4.0" =
-        "https://creativecommons.org/licenses/by-nd/4.0/"
+        "https://creativecommons.org/licenses/by-nd/4.0/legalcode"
     )
   )
   licenses <- structure(
